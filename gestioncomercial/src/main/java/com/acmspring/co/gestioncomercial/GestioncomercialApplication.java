6f39ce0346c2de11109b3c4b7323fea1a8e5f019
Edd022 <garciajulian258rgmail.com>
2025-11-22 22:03:27 -0500

    FEAT creación de docker compose y formato de proyecto

---
+++ gestioncomercial/src/main/java/com/acmspring/co/gestioncomercial/GestioncomercialApplication.java
@@ -0,0 +1,13 @@
+package com.acmspring.co.gestioncomercial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioncomercialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestioncomercialApplication.class, args);
+    }
+
+}
---
+++ gestioncomercial/src/test/java/com/acmspring/co/gestioncomercial/GestioncomercialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acmspring.co.gestioncomercial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioncomercialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
